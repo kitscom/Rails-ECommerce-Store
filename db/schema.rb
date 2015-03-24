d420958d7e768ea9593d75eb3878209c4e1064df
Amanda Henry <ahenryindigo@hotmail.com>
2015-03-24 09:32:01 -0500

    Made model for Province table.

--- db/schema.rb
+++ db/schema.rb
@@ -65,11 +65,6 @@ ActiveRecord::Schema.define(version: 20150324015837) do
     t.datetime "updated_at",     null: false
   end
 
-  create_table "resumes", force: :cascade do |t|
-    t.string   "name"
-    t.string   "attachment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
 end
