c1352cb9151518c481c6d5c823215b4eeea27625
Amanda Henry <ahenryindigo@hotmail.com>
2015-03-26 12:27:25 -0500

    Trying to move the province_id column to be after the city column in model Customer.

--- db/migrate/20150326134201_add_province_ref_to_customers.rb
+++ db/migrate/20150326134201_add_province_ref_to_customers.rb
@@ -1,6 +1,6 @@
 class AddProvinceRefToCustomers < ActiveRecord::Migration
   def change
-    add_reference :customers, :province, index: true
+    add_reference :customers, :province, index: true, :after => :username
     add_foreign_key :customers, :provinces
   end
 end
