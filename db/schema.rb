bd1f6a1d56843943ec874c1129fa6b9d455c142b
Amanda Henry <ahenryindigo@hotmail.com>
2015-03-27 08:53:24 -0500

    Made migration for foreign key in products and registered category with active_admin.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326163601) do
+ActiveRecord::Schema.define(version: 20150327134913) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -94,7 +94,10 @@ ActiveRecord::Schema.define(version: 20150326163601) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "image"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "provinces", force: :cascade do |t|
     t.string   "name"
