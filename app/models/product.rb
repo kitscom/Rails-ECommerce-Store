61e709a71eafc74de6ce239aa5b619f83116c8db
Amanda Henry <ahenryindigo@hotmail.com>
2015-03-27 08:50:42 -0500

    Made migration for foreignkey in products and registered category with active_admin.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   mount_uploader :image, ProductUploader
+  
+  belongs_to :category
   has_many :line_items
   has_many :orders, :through => :line_items
   validates :name, presence: true, uniqueness: true
