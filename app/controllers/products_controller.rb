2a921e492e8c9b7049910a21b4e07188b0078558
Amanda Henry <ahenryindigo@hotmail.com>
2015-03-30 13:22:45 -0500

    Added more products, removed products, added @page_title variable.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,42 +1,48 @@
 class ProductsController < ApplicationController
   def index
-   @products = Product.all.page(params[:page]).per(5)
+   @products = Product.all.page(params[:page]).per(10)
+   @page_title = "Main page title"
+   #render :index - default
   end  
   def currency (amount)
     sprintf("$%.2f",amount)
   end  
   def show
     @product = Product.find (params[:id])
-  end  
-  def new
-   # render text: "for testing purposes"
-   @product = Product.new
-  end  
-  #no associated view required
-  def create
-   @product = Product.new(product_params)   
-   if (@product.save)
-     redirect_to @product
-   else
-   render:new
-   end 
-  end  
-  def edit
-    @product = Product.find (params[:id])           
-  end  
-  def update
-    @product = Product.find(params[:id])
-    if (@product.update_attributes(product_params))
-      redirect_to @product
-    else
-      render :edit      
-    end 
-  end  
-  def destroy
-    @product = Product.find (params[:id])
-    @product.destroy
-    redirect_to root_path
-  end  
+    @page_title = "Show page title"
+  end
+  def category(args)
+    #@products = Product.category(:name, :description)
+  end
+  #def new
+  # # render text: "for testing purposes"
+  # @product = Product.new
+  #end  
+  ##no associated view required
+  #def create
+  # @product = Product.new(product_params)   
+  # if (@product.save)
+  #   redirect_to @product
+  # else
+  # render:new
+  # end 
+  #end  
+  #def edit
+  #  @product = Product.find (params[:id])           
+  #end  
+  #def update
+  #  @product = Product.find(params[:id])
+  #  if (@product.update_attributes(product_params))
+  #    redirect_to @product
+  #  else
+  #    render :edit      
+  #  end 
+  #end  
+  #def destroy
+  #  @product = Product.find (params[:id])
+  #  @product.destroy
+  #  redirect_to root_path
+  #end  
   private
   def product_params
     params.require(:product).permit(:name, :price, :description, :image)
